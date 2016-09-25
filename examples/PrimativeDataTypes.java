3deb511a4f7e690349ee440510d1d0a81e093c4d
Eddie Rangel <hacker@eddierangel.com>
2016-09-24 17:41:39 -0700

    Playing with Primatives

---
+++ examples/PrimativeDataTypes.java
@@ -0,0 +1,80 @@
+/*
+    An example of the Primative Data Types available in Java
+*/
+
+public class PrimativeDataTypes
+{
+    public static void main(String[] args)
+    {
+        boolean result = true; 
+        char capitalE = 'E';
+        byte aByte = 101; //binary value 101 decimal value 5
+        short aShort = 10000;
+        int anInt = 100000;
+
+        System.out.println(result);
+        System.out.println(capitalE);
+
+        System.out.println("Minimum value of a Byte: " + Byte.MIN_VALUE);
+        System.out.println("Maximum value of a Byte: " + Byte.MAX_VALUE);
+        System.out.println("Number of Bits: " + Byte.SIZE);
+        System.out.println("Number of Bytes: " + Byte.BYTES);
+
+        System.out.println("Minimum value of a Character: " + Character.MIN_VALUE);
+        System.out.println("Maximum value of a Character: " + Character.MAX_VALUE);
+        System.out.println("Number of Bits: " + Character.SIZE);
+        System.out.println("Number of Bytes: " + Character.BYTES);
+
+        System.out.println("Minimum value of a Short: " + Short.MIN_VALUE);
+        System.out.println("Maximum value of a Short: " + Short.MAX_VALUE);
+        System.out.println("Number of Bits: " + Short.SIZE);
+        System.out.println("Number of Bytes: " + Short.BYTES);
+
+        System.out.println("Minimum value of a Integer: " + Integer.MIN_VALUE);
+        System.out.println("Maximum value of a Integer: " + Integer.MAX_VALUE);
+        System.out.println("Number of Bits: " + Integer.SIZE);
+        System.out.println("Number of Bytes: " + Integer.BYTES);
+
+        System.out.println("Minimum value of a Double: " + Double.MIN_VALUE);
+        System.out.println("Maximum value of a Double: " + Double.MAX_VALUE);
+        System.out.println("Number of Bits: " + Double.SIZE);
+        System.out.println("Number of Bytes: " + Double.BYTES);
+
+        System.out.println("Minimum value of a Float: " + Float.MIN_VALUE);
+        System.out.println("Maximum value of a Float: " + Float.MAX_VALUE);
+        System.out.println("Number of Bits: " + Float.SIZE);
+        System.out.println("Number of Bytes: " + Float.BYTES);
+
+
+        System.out.println(aByte);
+        System.out.println(aShort);
+        System.out.println(anInt);
+
+        // The number 26, in decimal
+        int decVal = 26;
+        //  The number 26, in hexadecimal
+        int hexVal = 0x1a;
+        // The number 26, in binary
+        int binVal = 0b11010;
+
+        System.out.println(decVal);
+        System.out.println(hexVal);
+        System.out.println(binVal);
+
+        double d1 = 123.4;
+        // same value as d1, but in scientific notation
+        double d2 = 1.234e2;
+        float f1  = 123.4f;
+
+        long creditCardNumber = 1234_5678_9012_3456L;
+        long socialSecurityNumber = 999_99_9999L;
+        float pi =  3.14_15F;
+        long hexBytes = 0xFF_EC_DE_5E;
+        long hexWords = 0xCAFE_BABE;
+        long maxLong = 0x7fff_ffff_ffff_ffffL;
+        byte nybbles = 0b0010_0101;
+        long bytes = 0b11010010_01101001_10010100_10010010;
+
+
+    }
+}
